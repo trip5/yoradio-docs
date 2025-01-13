8a762e75d2b8737789ca459f0a2c7ac7e2152399
darkpoet78 <darkpoet@iname.com>
2025-01-13 23:44:22 +0900

    C3 flash fix, S3 description corrected

--- docs/ESP32-C3.js
+++ docs/ESP32-C3.js
@@ -1,5 +1,5 @@
 // This file handles which pins need to be banned from use for certain board types
-var BanPinsInfo = "Auxiliary section that allows you to prohibit the appointment of GPIOs 33-37 when choosing an ESP32-C3 module with PSRAM.";
-var noBanPinsLabel = "No PSRAM";
-var BanPinsLabel = "PSRAM";
-var BanPins = [33, 34, 35, 36, 37];
+var BanPinsInfo = "Auxiliary section that allows you to prohibit the appointment of GPIOs 11-17 when choosing an ESP32-C3 board with flash memory. These boards may have a designation like ESP32-C3Fx4 instead of just ESP32-C3.";
+var noBanPinsLabel = "No Flash Memory";
+var BanPinsLabel = "Flash Memory";
+var BanPins = [11, 12, 13, 14, 15, 16, 17];
